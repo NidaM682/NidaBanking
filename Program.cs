b3dfcd337a1eef813816875198cf9e5775ba7682
NidaM682 <144783333+NidaM682@users.noreply.github.com>
2023-09-13 13:30:49 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program{
+    static void DisplayAccountDetails(Account account2){
+        Console.WriteLine($"Account Number: {account2.AccountNumber}");
+        Console.WriteLine($"Balance: ${account2.Balance}");
+    }
+    static void Main(string [] args){
+        SavingsAccount savingsAccount = new SavingsAccount(1600, 78.0);
+        CheckingAccount checkingAccount = new CheckingAccount(2000,680.0);
+
+        savingsAccount.Deposit(2000);
+        DisplayAccountDetails(savingsAccount);
+        savingsAccount.CalculateInterest();
+        DisplayAccountDetails(savingsAccount);
+
+        Console.WriteLine();
+
+        checkingAccount.Deposit(100);
+        DisplayAccountDetails(checkingAccount);
+        checkingAccount.Withdraw(600);
+        DisplayAccountDetails(checkingAccount);
+    }
+}
